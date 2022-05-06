4119e5038c0eb063dcde999fee2350c88f516b71
brunomaria71 <brunomaria71@yahoo.com>
2022-05-06 14:40:39 -0400

    changed to es6

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,23 @@
-const functions = require("firebase-functions");
+import  functions from 'firebase-functions'
+import express from 'express';
+
+const app = express();
+
+app.get('/gotit', (req,res) => {
+    res.send('Wow I cannot believe this works!😱');
+});
+
+export const api = functions.https.onRequest(app);
+
+// const admin = require("firebase-admin");
+
+// const serviceAccount = require('./credentials.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+  
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
